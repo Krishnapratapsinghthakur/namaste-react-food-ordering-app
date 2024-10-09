de48827e6adb784033b178363cf21e3ec252bfbf
krishnapratapsingh <krishnathakur0182@gmail.com>
2024-10-09 21:57:39 +0530

    build the fetch function which call the api of swigggi and render the data dynamically to the web page and build the search functionlaty

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import reactDOM from 'react-dom/client'
 import Header from './src/Components/Header'
 import Body from './src/Components/Body'
 
+
 const Applayout=()=>{
 
     return(
         <div>
             <Header />
             <Body />
+            
             
         </div>
     ) 
--- src/Components/Rescontainer.js
+++ src/Components/Rescontainer.js
@@ -1,20 +1,88 @@
 import ResCard from "./Rescard";
-import restaurants from "../utils/hardcoded";
-import {useState} from 'react'
+
+import {useState,useEffect} from 'react'
+import Shimmer from "./Shimmer";
 
 
 
 
 
 const ResContainer=()=>{
-  const [restraunts1,setrestraunts1]=useState(restaurants)
+  const [restraunts1,setrestraunts1]=useState([])
+  const [restraunts,setrestraunts]=useState([])
+  const [Searchtext,setSearchtext]=useState("")
+
+  useEffect(()=>{
+    fetchdata()
+  },[])
+
+  fetchdata= async()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.71700&lng=75.83370&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+
+    const json =await data.json()
+
+    // console.log(json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    const apirestaurants = json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    // console.log(apirestaurants)
+    setrestraunts1(apirestaurants)
+    setrestraunts(apirestaurants)
+    // json.data.cards[1].card.card.girdElements.infoWithStyle.restaurants
+  }
+
+  if(restraunts1.length===0)
+  {
     return(
+    
+    <div className="shimmerdiv"><Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+    <Shimmer />
+</div>
+    ) 
+  }
+
+
+    return(
+
+
+
 
       
 
       
         <div >
-             <div className="search">
+             <div className="feature">
+              <div className="search">
+
+                <input
+                type="text"
+                value={Searchtext}
+                onChange={(e)=> {
+                  setSearchtext(e.target.value)
+                }}>
+                </input>
+                <button
+                className="searchbtn"
+                onClick={()=>{
+                  const filtered=restraunts.filter((res)=>res.info.name.toLowerCase().includes(Searchtext.toLowerCase()) )
+                setrestraunts1(filtered)
+                console.log(filtered)
+                }
+                }
+                >Search</button>
+
+              </div>
+
+
                 <button
                     className="btn"
                     onClick={()=>{
